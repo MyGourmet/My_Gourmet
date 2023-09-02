2db34bc1cc5ac2a0f67a6e4ffb48acf72b61433a
schwarzwald0906 <schwarzwald0906@gmail.com>
2023-09-02 14:43:39 +0900

    classifylogのドキュメントを生成

--- lib/classify_login_main.dart
+++ lib/classify_login_main.dart
@@ -2,12 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:my_gourmet/post.dart';
+import 'package:my_gourmet/classify_log.dart';
 import 'firebase_options.dart';
 import 'package:logger/logger.dart';
-import 'dart:convert';
 
 final Logger logger = Logger();
 Future<void> main() async {
@@ -68,12 +66,12 @@ class _SignInPageState extends State<SignInPage> {
             // ログインが成功すると FirebaseAuth.instance.currentUser にログイン中のユーザーの情報が入ります
             print(FirebaseAuth.instance.currentUser?.displayName);
 
-            // ログインに成功したら ChatPage に遷移します。
+            // ログインに成功したら ClassifyLogPage に遷移します。
             // 前のページに戻らせないようにするにはpushAndRemoveUntilを使います。
             if (mounted) {
               Navigator.of(context).pushAndRemoveUntil(
                 MaterialPageRoute(builder: (context) {
-                  return const ChatPage();
+                  return const ClassifyLogPage();
                 }),
                 (route) => false,
               );
@@ -85,37 +83,39 @@ class _SignInPageState extends State<SignInPage> {
   }
 }
 
-final postsReference =
-    FirebaseFirestore.instance.collection('posts').withConverter<Post>(
-  // <> ここに変換したい型名をいれます。今回は Post です。
+final classifylogsReference = FirebaseFirestore.instance
+    .collection('classifylogs')
+    .withConverter<ClassifyLog>(
+  // <> ここに変換したい型名をいれます。今回は ClassifyLog です。
   fromFirestore: ((snapshot, _) {
     // 第二引数は使わないのでその場合は _ で不使用であることを分かりやすくしています。
-    return Post.fromFirestore(snapshot); // 先ほど定期着した fromFirestore がここで活躍します。
+    return ClassifyLog.fromFirestore(
+        snapshot); // 先ほど定期着した fromFirestore がここで活躍します。
   }),
   toFirestore: ((value, _) {
     return value.toMap(); // 先ほど適宜した toMap がここで活躍します。
   }),
 );
 
-class ChatPage extends StatefulWidget {
-  const ChatPage({Key? key}) : super(key: key);
+class ClassifyLogPage extends StatefulWidget {
+  const ClassifyLogPage({Key? key}) : super(key: key);
 
   @override
-  State<ChatPage> createState() => _ChatPageState();
+  State<ClassifyLogPage> createState() => _ClassifyLogPageState();
 }
 
-class _ChatPageState extends State<ChatPage> {
+class _ClassifyLogPageState extends State<ClassifyLogPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('チャット'),
+        title: const Text('グルメ'),
       ),
       body: Column(children: [
         Expanded(
-          child: StreamBuilder<QuerySnapshot<Post>>(
+          child: StreamBuilder<QuerySnapshot<ClassifyLog>>(
             // stream プロパティに snapshots() を与えると、コレクションの中のドキュメントをリアルタイムで監視することができます。
-            stream: postsReference.orderBy('createdAt').snapshots(),
+            stream: classifylogsReference.orderBy('createdAt').snapshots(),
             // ここで受け取っている snapshot に stream で流れてきたデータが入っています。
             builder: (context, snapshot) {
               // docs には Collection に保存されたすべてのドキュメントが入ります。
@@ -125,11 +125,11 @@ class _ChatPageState extends State<ChatPage> {
               return ListView.builder(
                 itemCount: docs.length,
                 itemBuilder: (context, index) {
-                  // data() に Post インスタンスが入っています。
+                  // data() に ClassifyLog インスタンスが入っています。
                   // これは withConverter を使ったことにより得られる恩恵です。
                   // 何もしなければこのデータ型は Map になります。
-                  final post = docs[index].data();
-                  return Text(post.text);
+                  final classifyLog = docs[index].data();
+                  return Text(classifyLog.state);
                 },
               );
             },
@@ -140,27 +140,25 @@ class _ChatPageState extends State<ChatPage> {
             // まずは user という変数にログイン中のユーザーデータを格納します
             final user = FirebaseAuth.instance.currentUser!;
 
-            final posterId = user.uid; // ログイン中のユーザーのIDがとれます
-            final posterName = user.displayName!; // Googleアカウントの名前がとれます
-            final posterImageUrl = user.photoURL!; // Googleアカウントのアイコンデータがとれます
+            final userId = user.uid; // ログイン中のユーザーのIDがとれます
+            final loggerName = user.displayName!; // Googleアカウントの名前がとれます
+            final loggerImageUrl = user.photoURL!; // Googleアカウントのアイコンデータがとれます
 
-            // 先ほど作った postsReference からランダムなIDのドキュメントリファレンスを作成します
+            // 先ほど作った classifylogsReference からランダムなIDのドキュメントリファレンスを作成します
             // doc の引数を空にするとランダムなIDが採番されます
-            final newDocumentReference = postsReference.doc();
+            final newDocumentReference = classifylogsReference.doc();
 
-            final newPost = Post(
-              text: text,
-              createdAt: Timestamp.now(), // 投稿日時は現在とします
-              posterName: posterName,
-              posterImageUrl: posterImageUrl,
-              posterId: posterId,
-              reference: newDocumentReference,
-            );
+            final newClassifyLog = ClassifyLog(
+                createdAt: Timestamp.now(),
+                updatedAt: Timestamp.now(),
+                userId: userId,
+                state: text,
+                reference: newDocumentReference);
 
             // 先ほど作った newDocumentReference のset関数を実行するとそのドキュメントにデータが保存されます。
-            // 引数として Post インスタンスを渡します。
-            // 通常は Map しか受け付けませんが、withConverter を使用したことにより Post インスタンスを受け取れるようになります。
-            newDocumentReference.set(newPost);
+            // 引数として ClassifyLog インスタンスを渡します。
+            // 通常は Map しか受け付けませんが、withConverter を使用したことにより ClassifyLog インスタンスを受け取れるようになります。
+            newDocumentReference.set(newClassifyLog);
           },
         ),
       ]),
