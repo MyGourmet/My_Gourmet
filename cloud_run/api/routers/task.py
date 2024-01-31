d5e253c64e955ca2a426ac8a10b0b6d20c04b41f
schwarzwald0906 <schwarzwald0906@gmail.com>
2024-01-31 09:41:16 +0900

    不要な処理を削除

--- cloud_run/api/routers/task.py
+++ cloud_run/api/routers/task.py
@@ -34,23 +34,3 @@ async def update_user_status_endpoint(request: Request, db=Depends(get_firestore
     # update_user_status関数を呼び出し、dbを引数として渡す
     return update_user_status(user_id=user_id, access_token=access_token, db=db)
 
-
-
-@router.get("/tasks", response_model=List[task_schema.Task])
-async def list_tasks():
-    return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
-
-
-@router.post("/tasks", response_model=task_schema.TaskCreateResponse)
-async def create_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
-
-
-@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_id: int, task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
-
-
-@router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int):
-    return
