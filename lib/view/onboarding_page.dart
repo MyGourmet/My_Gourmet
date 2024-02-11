c465f35278297748fa350b59d95ef1175850dd04
masaki <masakisato.japan@gmail.com>
2024-02-11 12:33:25 +0900

    refactor onboarding_page
    Since it's better to see whether to show on_boarding_page or not from home page, manage the state with the state provider

--- lib/view/onboarding_page.dart
+++ lib/view/onboarding_page.dart
@@ -6,12 +6,27 @@ import '../core/app_colors.dart';
 import '../core/build_context_extension.dart';
 import '../core/constants.dart';
 import '../core/shared_preferences_service.dart';
-import 'home_page.dart';
 import 'widgets/buttons.dart';
 
-/// オンボーディング用画面
+/// オンボーディング完了フラグ用[StateProvider]
 ///
-/// [SharedPreferencesService]を参照して[HomePage]上に表示するかどうかを判断する。
+///  外部から更新をすることで[SharedPreferencesService]側の値も更新する。
+final isOnBoardingCompletedProvider = StateProvider<bool>((ref) {
+  final sharedPreferencesService = ref.watch(sharedPreferencesServiceProvider);
+
+  ref.listenSelf((_, next) {
+    sharedPreferencesService.setBool(
+      key: SharedPreferencesKey.isOnboardingCompleted,
+      value: next,
+    );
+  });
+
+  return sharedPreferencesService.getBool(
+    key: SharedPreferencesKey.isOnboardingCompleted,
+  );
+});
+
+/// オンボーディング用画面
 class OnboardingPage extends ConsumerStatefulWidget {
   const OnboardingPage({super.key});
 
@@ -22,7 +37,6 @@ class OnboardingPage extends ConsumerStatefulWidget {
 class _OnboardingPageState extends ConsumerState<OnboardingPage> {
   late PageController _onboardingController;
   int currentOnboarding = 0;
-  late SharedPreferencesService _sharedPreferencesService;
 
   @override
   void initState() {
@@ -43,90 +57,79 @@ class _OnboardingPageState extends ConsumerState<OnboardingPage> {
     final screenHeight = context.deviceHeight;
     final isOnboardingTop = currentOnboarding == 0;
     final isNotLastOnboarding = currentOnboarding != 3;
-    _sharedPreferencesService = ref.watch(sharedPreferencesServiceProvider);
-    var isOnboardingCompleted = _sharedPreferencesService.getBool(
-      key: SharedPreferencesKey.isOnboardingCompleted,
-    );
 
-    return Visibility(
-      visible: !isOnboardingCompleted,
-      child: GestureDetector(
-        // オンボーディング初めのロゴ画面の場合、画面タップで遷移させる
-        onTap: isOnboardingTop
-            ? () {
-                _onboardingController.nextPage(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.linear,
-                );
-              }
-            : null,
-        child: Container(
-          color: AppColors.black.withOpacity(0.9),
-          width: screenWidth,
-          height: screenHeight,
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            children: [
-              Flexible(
-                child: PageView(
-                  controller: _onboardingController,
-                  children: <Widget>[
-                    Center(
-                      child: Image.asset(
-                        'assets/images/top_image.png',
-                        width: screenWidth / 2,
-                      ),
+    return GestureDetector(
+      // オンボーディング初めのロゴ画面の場合、画面タップで遷移させる
+      onTap: isOnboardingTop
+          ? () {
+              _onboardingController.nextPage(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.linear,
+              );
+            }
+          : null,
+      child: Container(
+        color: AppColors.black.withOpacity(0.9),
+        width: screenWidth,
+        height: screenHeight,
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            Flexible(
+              child: PageView(
+                controller: _onboardingController,
+                children: <Widget>[
+                  Center(
+                    child: Image.asset(
+                      'assets/images/top_image.png',
+                      width: screenWidth / 2,
                     ),
-                    const _OnboardingContent(
-                      title: 'せっかく撮った食べ物の写真\nそのままになっていませんか？\n',
-                      description:
-                          // ignore: lines_longer_than_80_chars
-                          '素敵なごはんやお店の写真が\nフォルダ内であちこちにあったり。\nせっかく撮ってもそれっきりで\n見返すことがなかったり。',
-                      imagePath: 'assets/images/onboarding1.png',
-                      imageWidth: 160,
-                    ),
-                    const _OnboardingContent(
-                      title: 'あなたの画像ライブラリから\n食べ物の写真のみを\n自動ピックアップします！',
-                      description:
-                          // ignore: lines_longer_than_80_chars
-                          'MyGourmetではあなたの画像フォルダの\nすべての画像を自動で解析し\n食べ物の写真のみ表示させることができます。',
-                      imagePath: 'assets/images/onboarding2.png',
-                      imageWidth: 220,
-                    ),
-                    const _OnboardingContent(
-                      title: 'あなただけの\n食の思い出アルバムが\n出来上がります！',
-                      description:
-                          // ignore: lines_longer_than_80_chars
-                          'それぞれの料理の味わいや体験を\nひと目で見返せるようになります。\n\nさあ、さっそく今までの写真を\n記録してみましょう！',
-                      imagePath: 'assets/images/onboarding3.png',
-                      imageWidth: 140,
-                    ),
-                  ],
-                ),
+                  ),
+                  const _OnboardingContent(
+                    title: 'せっかく撮った食べ物の写真\nそのままになっていませんか？\n',
+                    description: // ignore: lines_longer_than_80_chars
+                        '素敵なごはんやお店の写真が\nフォルダ内であちこちにあったり。\nせっかく撮ってもそれっきりで\n見返すことがなかったり。',
+                    imagePath: 'assets/images/onboarding1.png',
+                    imageWidth: 160,
+                  ),
+                  const _OnboardingContent(
+                    title: 'あなたの画像ライブラリから\n食べ物の写真のみを\n自動ピックアップします！',
+                    description:
+                        // ignore: lines_longer_than_80_chars
+                        'MyGourmetではあなたの画像フォルダの\nすべての画像を自動で解析し\n食べ物の写真のみ表示させることができます。',
+                    imagePath: 'assets/images/onboarding2.png',
+                    imageWidth: 220,
+                  ),
+                  const _OnboardingContent(
+                    title: 'あなただけの\n食の思い出アルバムが\n出来上がります！',
+                    description:
+                        // ignore: lines_longer_than_80_chars
+                        'それぞれの料理の味わいや体験を\nひと目で見返せるようになります。\n\nさあ、さっそく今までの写真を\n記録してみましょう！',
+                    imagePath: 'assets/images/onboarding3.png',
+                    imageWidth: 140,
+                  ),
+                ],
               ),
-              if (!isOnboardingTop)
-                CustomButton.orange(
-                  onPressed: () async {
-                    if (isNotLastOnboarding) {
-                      await _onboardingController.nextPage(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.linear,
-                      );
-                    } else {
-                      // オンボーディング完了フラグをたてる
-                      isOnboardingCompleted =
-                          await _sharedPreferencesService.setBool(
-                        key: SharedPreferencesKey.isOnboardingCompleted,
-                        value: true,
-                      );
-                      setState(() {});
-                    }
-                  },
-                  text: isNotLastOnboarding ? 'つぎへ' : 'はじめる',
-                ),
-              const Gap(32),
-            ],
-          ),
+            ),
+            if (!isOnboardingTop)
+              CustomButton.orange(
+                onPressed: () async {
+                  if (isNotLastOnboarding) {
+                    await _onboardingController.nextPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.linear,
+                    );
+                  } else {
+                    // オンボーディング完了フラグをたてる
+                    ref
+                        .read(isOnBoardingCompletedProvider.notifier)
+                        .update((state) => true);
+                  }
+                },
+                text: isNotLastOnboarding ? 'つぎへ' : 'はじめる',
+              ),
+            const Gap(32),
+          ],
         ),
       ),
     );
