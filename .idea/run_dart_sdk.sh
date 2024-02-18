2d5a038b44f611c8e57de3b42c1b77ad7d5c793a
masaki <masakisato.japan@gmail.com>
2024-02-18 15:39:35 +0900

    fix the file watcher to adapt to fvm ver 3.0

--- .idea/run_dart_sdk.sh
+++ .idea/run_dart_sdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # fvm_config.jsonからFlutterバージョンを取得
-FLUTTER_VERSION=$(cat ".fvm/fvm_config.json" | grep flutterSdkVersion | cut -d '"' -f 4)
+FLUTTER_VERSION=$(cat "./.fvmrc" | grep flutter | cut -d '"' -f 4)
 
 # 'fvm/versions'ディレクトリがホームディレクトリ配下にあることを前提にしている
 FVM_VERSIONS_DIR="$HOME/fvm/versions"
