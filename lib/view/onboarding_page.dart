8d8e26224db0913908e5c9cd3a29b12ca9604d76
sasatank <sasa.tankytank@gmail.com>
2024-05-16 22:01:11 +0900

    ボタンとインジケーター修正

--- lib/view/onboarding_page.dart
+++ lib/view/onboarding_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-import '../core/build_context_extension.dart';
 import '../core/shared_preferences_service.dart';
+import '../core/themes.dart';
 
 /// オンボーディング完了フラグ用[StateProvider]
 ///
-///  外部から更新をすることで[SharedPreferencesService]側の値も更新する。
+/// 外部から更新をすることで[SharedPreferencesService]側の値も更新する。
 final isOnBoardingCompletedProvider = StateProvider<bool>((ref) {
   final sharedPreferencesService = ref.watch(sharedPreferencesServiceProvider);
 
@@ -31,80 +32,141 @@ class OnboardingPage extends ConsumerStatefulWidget {
 }
 
 class _OnboardingPageState extends ConsumerState<OnboardingPage> {
-  late PageController _onboardingController;
+  final PageController _pageController = PageController();
   int currentOnboarding = 0;
 
   @override
   void initState() {
     super.initState();
-    // 初期化
-    _onboardingController = PageController()
-      ..addListener(() {
-        final page = _onboardingController.page!.round();
-        setState(() {
-          currentOnboarding = page;
-        });
+    _pageController.addListener(() {
+      final page = _pageController.page!.round();
+      setState(() {
+        currentOnboarding = page;
       });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final isLastOnboarding = currentOnboarding == 2;
+    final isLastPage = currentOnboarding == 2; // 最後のページ判定
 
-    return GestureDetector(
-      onTap: isLastOnboarding
-          ? () {
-              ref
-                  .read(isOnBoardingCompletedProvider.notifier)
-                  .update((state) => true);
-            }
-          : () {
-              _onboardingController.nextPage(
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.linear,
-              );
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          PageView(
+            controller: _pageController, // PageControllerをPageViewに接続
+            onPageChanged: (int page) {
+              setState(() {
+                currentOnboarding = page;
+              });
             },
-      child: SizedBox(
-        width: context.screenWidth,
-        height: context.screenHeight,
-        child: Column(
-          children: [
-            Flexible(
-              child: PageView(
-                controller: _onboardingController,
-                children: const <Widget>[
-                  _OnboardingContent(
-                    imagePath: 'assets/images/new_onboarding1.png',
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/new_onboarding1.png'),
+                    fit: BoxFit.cover,
                   ),
-                  _OnboardingContent(
-                    imagePath: 'assets/images/new_onboarding2.png',
-                  ),
-                  _OnboardingContent(
-                    imagePath: 'assets/images/new_onboarding3.png',
-                  ),
-                ],
+                ),
               ),
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/new_onboarding2.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/new_onboarding3.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: SmoothPageIndicator(
+                      controller: _pageController, // PageControllerを渡す
+                      count: 3, // ページの数を3に設定
+                      effect: WormEffect(
+                        dotHeight: 12, // 点の高さを小さくする
+                        dotWidth: 12, // 点の幅を小さくする
+                        spacing: 23,
+                        activeDotColor: Colors.grey[800]!, // アクティブな点の色を濃い灰色にする
+                        dotColor: Colors.grey[400]!, // 非アクティブな点の色を薄い灰色にする
+                      ), // インジケータのエフェクト
+                      onDotClicked: (index) {
+                        _pageController.animateToPage(
+                          index,
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(height: 28),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.05,
+                  ),
+                  child: Container(
+                    height: 60, // お好みの高さに設定してください
+                    decoration: BoxDecoration(
+                      color: Themes.mainOrange, // 背景色
+                      borderRadius: BorderRadius.circular(8.0), // 角の丸み
+                    ),
+                    child: InkWell(
+                      onTap: () {
+                        if (!isLastPage) {
+                          _pageController.nextPage(
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.easeInOut,
+                          );
+                        } else {
+                          ref
+                              .read(isOnBoardingCompletedProvider.notifier)
+                              .update((state) => true);
+                        }
+                      },
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                isLastPage ? 'はじめる' : 'やってみる',
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 43),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-    );
-  }
-}
-
-class _OnboardingContent extends StatelessWidget {
-  const _OnboardingContent({
-    required this.imagePath,
-  });
-
-  final String imagePath;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Image.asset(imagePath, fit: BoxFit.cover),
     );
   }
 }
