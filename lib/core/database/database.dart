fb1ad008e0b876e7fe7f866492df576e27dee0f5
タオル <yoseimine0528@gmail.com>
2024-05-17 21:48:06 +0900

    loggerを削除。

--- lib/core/database/database.dart
+++ lib/core/database/database.dart
@@ -2,13 +2,10 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
-import 'package:logger/logger.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 part 'database.g.dart';
-
-Logger logger = Logger();
 
 /// 写真テーブル
 @DataClassName('Photo')
