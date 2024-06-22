bdd2a21101cdc9dff40e3a1705a6416c56f3b7c3
sasatank <sasa.tankytank@gmail.com>
2024-06-22 13:40:16 +0900

    オンボーディング時に画像追加からスタートするように変更

--- lib/view/onboarding_page.dart
+++ lib/view/onboarding_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:go_router/go_router.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../core/build_context_extension.dart';
 import '../core/shared_preferences_service.dart';
