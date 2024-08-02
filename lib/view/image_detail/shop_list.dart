01bb8c72184c470da179c6b347c027d51adf1683
masaki <masakisato.japan@gmail.com>
2024-08-02 22:39:43 +0900

    fix: ダイアログの返り値を修正
    何も返していないためvoidへ変更

--- lib/view/image_detail/shop_list.dart
+++ lib/view/image_detail/shop_list.dart
@@ -7,19 +7,17 @@ import '../../core/themes.dart';
 import '../widgets/custom_elevated_button.dart';
 import '../widgets/scalable_image.dart';
 
-
 List<File> shopList = [];
 int shopNo = 0;
 int shopNoSelected = 0;
 
-Future<dynamic> showShopListDialog (
-  BuildContext context, { 
-    required String shopName,
-    required List<File> imageFileList,
-    required void Function() onSelected,
-  
-}) async { 
-  await showDialog<dynamic>(
+Future<void> showShopListDialog(
+  BuildContext context, {
+  required String shopName,
+  required List<File> imageFileList,
+  required void Function() onSelected,
+}) async {
+  await showDialog<void>(
     context: context,
     barrierDismissible: false,
     builder: (BuildContext context) {
@@ -36,7 +34,7 @@ Future<dynamic> showShopListDialog (
           ),
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            children:[
+            children: [
               Align(
                 alignment: Alignment.topRight,
                 child: Container(
@@ -46,95 +44,96 @@ Future<dynamic> showShopListDialog (
                     color: Color(0xFFfff4eb),
                     shape: BoxShape.circle,
                   ),
-                  child: IconButton(  
+                  child: IconButton(
                     onPressed: () {
                       Navigator.of(context).pop();
-                    },    
+                    },
                     icon: Icon(Icons.close, size: 20, color: Colors.grey[800]),
                   ),
                 ),
-              ), 
-              const SizedBox(height: 8),
-              Text (
-                '写真を撮った店舗を選んでください', 
-                style: context.textTheme.titleSmall, 
               ),
-              const SizedBox(height:10),
-              for(shopNo=0; shopNo < 3; shopNo++) ... {
-                Stack( 
-                  children: [ 
-                      Container(
-                        margin: const EdgeInsets.only(
-                          top: 7,
-                          bottom: 7,
-                          left: 10,
-                          right: 10,
-                        ),
-                        padding: const EdgeInsets.only(
-                          top: 5,
-                          bottom: 10,
-                        ),                       
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 2,
-                            color: shopNoSelected == shopNo 
-                            ? Themes.mainOrange
-                            : const Color(0xFFd8cac1),
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5), 
-                              blurRadius: 3, 
-                              offset: const Offset(0, 5), 
-                            ),
-                          ],
-                          color: Colors.white,
-                        ), 
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                left: 10, bottom: 5,
-                              ),
-                              child: Align(
-                                alignment: Alignment.topLeft,
-                                child: Text(
-                                  shopName,
-                                  style: context.textTheme.bodySmall,
-                                ),
-                              ),
-                            ), 
-                            SizedBox(
-                              height: 110,
-                              child: Scrollbar(
-                                child: ListView.builder(
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: imageFileList.length,
-                                  itemBuilder: (context, index) {
-                                    return Padding(
-                                      padding: const EdgeInsets.only(left: 10),
-                                      child: ClipRRect(
-                                        borderRadius: BorderRadius.circular(0),
-                                        child: ScalableImage(
-                                          imageFile: imageFileList[index],
-                                          height: 100,
-                                          width: 125,
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+              const SizedBox(height: 8),
+              Text(
+                '写真を撮った店舗を選んでください',
+                style: context.textTheme.titleSmall,
+              ),
+              const SizedBox(height: 10),
+              for (shopNo = 0; shopNo < 3; shopNo++) ...{
+                Stack(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(
+                        top: 7,
+                        bottom: 7,
+                        left: 10,
+                        right: 10,
                       ),
+                      padding: const EdgeInsets.only(
+                        top: 5,
+                        bottom: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 2,
+                          color: shopNoSelected == shopNo
+                              ? Themes.mainOrange
+                              : const Color(0xFFd8cac1),
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            blurRadius: 3,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                        color: Colors.white,
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 10,
+                              bottom: 5,
+                            ),
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: Text(
+                                shopName,
+                                style: context.textTheme.bodySmall,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 110,
+                            child: Scrollbar(
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: imageFileList.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(left: 10),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(0),
+                                      child: ScalableImage(
+                                        imageFile: imageFileList[index],
+                                        height: 100,
+                                        width: 125,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                     if (shopNo == shopNoSelected)
                       Positioned(
                         right: 0,
                         top: 0,
-                        child: Container( 
+                        child: Container(
                           width: 28,
                           height: 28,
                           decoration: const BoxDecoration(
@@ -142,34 +141,36 @@ Future<dynamic> showShopListDialog (
                             shape: BoxShape.circle,
                           ),
                           child: const Icon(
-                            Icons.check, 
+                            Icons.check,
                             color: Colors.white,
-                            size: 20, 
+                            size: 20,
                           ),
                         ),
-                       ),
-                  ],  
+                      ),
+                  ],
                 ),
               },
-            const SizedBox(height: 10),
-            const Align(
-              alignment: Alignment.centerRight,
-              child: Text('次の３店鋪'),
-            ),            
-            const SizedBox(height: 15),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.05,
+              const SizedBox(height: 10),
+              const Align(
+                alignment: Alignment.centerRight,
+                child: Text('次の３店鋪'),
               ),
-              child: SizedBox(
-                height: 60,
-                child: CustomElevatedButton(
-                  onPressed: onSelected,
-                  text: '決定',
+              const SizedBox(height: 15),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.05,
+                ),
+                child: SizedBox(
+                  height: 60,
+                  child: CustomElevatedButton(
+                    onPressed: onSelected,
+                    text: '決定',
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 15,),
+              const SizedBox(
+                height: 15,
+              ),
             ],
           ),
         ),
