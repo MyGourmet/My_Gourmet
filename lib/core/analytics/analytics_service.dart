45387c271fc1808b00053e400d531eb9496460f0
masaki <masakisato.japan@gmail.com>
2024-09-08 10:44:49 +0900

    fix: 不要なパラメータの削除
    併せて、別実装で不要だったコメントの削除や説明コメントの追加をした。尚、flavorは環境とfirebaseプロジェクトが1対１対応のため実質不要だが、念の為の動作チェック的意味合い & 分析する際の体験が良くなるかもしれないという意味合いで残している。

--- lib/core/analytics/analytics_service.dart
+++ lib/core/analytics/analytics_service.dart
@@ -6,52 +6,41 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../core/flavor.dart';
 import '../../features/auth/auth_repository.dart';
 
+/// [AnalyticsService]を提供する[Provider]
 final analyticsServiceProvider = Provider((ref) {
   // パラメータに付与するユーザー情報の取得
   final authUser = ref.watch(authRepositoryProvider).auth;
 
   return AnalyticsService(
     // 全てのユーザーから取得する初期値
-    // 'type': 'easy' 「かんたん設定」
-    // 'env' : 環境（flavor）の値
-    // 他ユーザー情報
     parameters: {
-      'type': 'easy',
       'env': flavor.name,
-
-      // analyticsで自動で取得されるパラメータがスネークケースだったのでそちらに合わせた。
-
-      ////////下記を改修
       'u_id': authUser.currentUser?.uid ?? '',
       'display_name': authUser.currentUser?.displayName ?? '',
-      // 'application_id': authUser?.applicationId ?? '',
-      // 'display_name': authUser?.displayName ?? '',
     },
   );
 });
 
+/// Analyticsに関する操作を担当するクラス
+///
+/// 参考)
+/// https://www.kamo-it.org/blog/flutter-analytics/
 class AnalyticsService {
   AnalyticsService({required Map<String, Object?> parameters})
       : _parameters = parameters;
-  static final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
-  // パラメータ
+
+  /// [FirebaseAnalytics]のインスタンス
+  static final FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
+
+  /// スネークケースのパラメータ
   final Map<String, Object?> _parameters;
 
   /// どの画面を開いているか、Analyticsにログを送信するメソッド
-  /// setCurrentScreenにはparameterを設定できないので、同時にlogEventも発火している。
+  ///
   /// logEvent name : screen_view_event
+  /// setCurrentScreenにはparameterを設定できないので、同時にlogEventも発火している。
   /// 参考)
   /// https://www.kamo-it.org/blog/flutter-analytics/
-  /// parameter例）
-  ///　{
-  ///　  type: easy,
-  ///　  env: local,
-  ///　  application_id: c0a5979b-6418-4a97-b783-f9c360220270,
-  ///　  display_name: docodoor_abeabe,
-  ///　  screen_name: /complete
-  ///　}
-  ///
-
   void sendScreenView(String path) {
     _parameters
       // event_nameが残っていたら余計な情報なので省く
@@ -59,26 +48,15 @@ class AnalyticsService {
       ..addAll({'screen_name': path});
     // パラメータの内容の確認
     log('Analyticsのパラメータ : $_parameters');
-    // {type: easy, env: local, applicationId: c0a5979b-6418-4a97-b783-f9c360220270, displayName: docodoor_abeabe, screen_name: /complete}
-    analytics
+
+    _analytics
       ..logEvent(name: 'screen_view_event', parameters: _parameters)
       ..logScreenView(screenName: path);
   }
 
   /// どのボタンを押したか、Analyticsに送信するメソッド
-  /// logEvent name : button_event
-  /// 参考)
-  /// https://www.kamo-it.org/blog/flutter-analytics/
-  /// parameter例）
-  ///　{
-  ///　  type: easy,
-  ///　  env: local,
-  ///　  button_name: next,
-  ///　  application_id: c0a5979b-6418-4a97-b783-f9c360220270,
-  ///　  display_name: docodoor_abeabe,
-  ///　  screen_name: /complete
-  ///　}
   ///
+  /// logEvent name : event_name
   Future<void> sendEvent({
     required String name,
     Map<String, Object?> addParameters = const {},
@@ -88,13 +66,10 @@ class AnalyticsService {
       ..addAll({'event_name': name})
       ..addAll(addParameters);
 
-    // Google Analyticsにイベントを送信
-    await analytics.logEvent(name: name, parameters: _parameters);
+    await _analytics.logEvent(name: name, parameters: _parameters);
   }
 
-  /// Analyticsに一律追加したいパラメータの設定
-  /// 例）旧管理画面から画面遷移した場合などに利用
-  ///
+  /// Analyticsに一律追加したいパラメータを設定するメソッド
   void setAddParameters({
     required Map<String, Object?> addParameters,
   }) {
